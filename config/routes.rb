e90a31667503e3c7bb8283c61141dec1d260e98e
poccob <poccob@mail.ru>
2017-12-26 22:36:42 +0800

    add controllers user

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
 
+
   root 'users#index'
 
-  resources :users
+  resources :users, except: [:destroy]
+  resources :sessions, only: [:new, :create, :destroy]
   resources :questions
+
+  get 'sign_up' => 'users#new'
+  get 'log_out' => 'sessions#destroy'
+  get 'log_in' => 'sessions#new'
 
   get 'show' => 'users#show'
 
