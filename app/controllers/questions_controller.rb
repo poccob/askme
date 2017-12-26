c6dd183421173999a31100ca6eed85689f4f35bc
poccob <poccob@mail.ru>
2017-12-26 23:21:51 +0800

    add controller question

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,61 @@
+class QuestionsController < ApplicationController
+  before_action :load_question, only: [:show, :edit, :update, :destroy]
+
+  before_action :authorize_user, except: [:create]
+  # GET /questions/1/edit
+  def edit
+  end
+
+  # POST /questions
+  def create
+    @question = Question.new(question_params)
+
+    if @question.save
+      redirect_to user_path(@question.user), notice: 'Вопрос задан'
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /questions/1
+  def update
+    if @question.update(question_params)
+      redirect_to user_path(@question.user), notice: 'Вопрос сохранен'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /questions/1
+  def destroy
+    user = @question.user
+    @question.destroy
+    redirect_to user_path(user), notice: 'Вопрос удален'
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def load_question
+    @question = Question.find(params[:id])
+  end
+
+  def authorize_user
+    reject_user unless @question.user == current_user
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  # Явно задаем список разрешенных параметров для модели Question. Мы говорим,
+  # что у хэша params должен быть ключ :question. Значением этого ключа может
+  # быть хэш с ключами: :user_id и :text. Другие ключи будут отброшены.
+  def question_params
+    # Защита от уязвимости: если текущий пользователь — адресат вопроса,
+    # он может менять ответы на вопрос, ему доступно также поле :answer.
+    if current_user.present? &&
+        params[:question][:user_id].to_i == current_user.id
+      params.require(:question).permit(:user_id, :text, :answer)
+    else
+      params.require(:question).permit(:user_id, :text)
+    end
+  end
+end
